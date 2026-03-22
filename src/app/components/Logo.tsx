5da9cd1674f745da4c7f7a2ef076b36049c72617
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2026-03-22 02:18:51 -0300

    componente da header

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,5 +1,3 @@
-import svgPaths from "../../assets/svg-61drw48kvb";
-
 interface LogoProps {
   onClick?: () => void;
 }
@@ -13,53 +11,19 @@ export function Logo({ onClick }: LogoProps) {
         left: "50%",
         top: "50%",
         transform: "translate(-50%, -50%)",
-        width: "127.624px",
-        height: "14px",
-        overflow: "hidden",
         cursor: "pointer",
         flexShrink: 0,
+        fontSize: "20px",
+        fontWeight: 600,
+        letterSpacing: "3.2px",
+        color: "#262626",
+        fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+        textTransform: "uppercase",
+        lineHeight: 1,
+        whiteSpace: "nowrap",
       }}
     >
-      <div style={{ position: "absolute", inset: "-0.01% 51.61% 0.04% 38.8%" }}>
-        <svg style={{ position: "absolute", display: "block", width: "100%", height: "100%" }} fill="none" preserveAspectRatio="none" viewBox="0 0 12.2316 13.9957">
-          <path d={svgPaths.p246e4cf0} fill="#262626" />
-        </svg>
-      </div>
-      <div style={{ position: "absolute", inset: "0.07% 13.2% 0.02% 77.04%" }}>
-        <svg style={{ position: "absolute", display: "block", width: "100%", height: "100%" }} fill="none" preserveAspectRatio="none" viewBox="0 0 12.4514 13.988">
-          <path d={svgPaths.p1861080} fill="#262626" />
-        </svg>
-      </div>
-      <div style={{ position: "absolute", inset: "0.25% 65.64% 0.3% 25.72%" }}>
-        <svg style={{ position: "absolute", display: "block", width: "100%", height: "100%" }} fill="none" preserveAspectRatio="none" viewBox="0 0 11.0277 13.9226">
-          <path d={svgPaths.p20e97200} fill="#262626" />
-        </svg>
-      </div>
-      <div style={{ position: "absolute", inset: "0.48% -0.01% 0.32% 91.35%" }}>
-        <svg style={{ position: "absolute", display: "block", width: "100%", height: "100%" }} fill="none" preserveAspectRatio="none" viewBox="0 0 11.0426 13.8876">
-          <path d={svgPaths.pb2ee500} fill="#262626" />
-        </svg>
-      </div>
-      <div style={{ position: "absolute", inset: "0.37% 91.34% 0.46% 0" }}>
-        <svg style={{ position: "absolute", display: "block", width: "100%", height: "100%" }} fill="none" preserveAspectRatio="none" viewBox="0 0 11.0601 13.8826">
-          <path d={svgPaths.p38d8a00} fill="#262626" />
-        </svg>
-      </div>
-      <div style={{ position: "absolute", inset: "-0.04% 26.24% -0.02% 62.84%" }}>
-        <svg style={{ position: "absolute", display: "block", width: "100%", height: "100%" }} fill="none" preserveAspectRatio="none" viewBox="0 0 13.9351 14.0091">
-          <path d={svgPaths.p2ccb0600} fill="#262626" />
-        </svg>
-      </div>
-      <div style={{ position: "absolute", inset: "0.06% 77.33% -0.02% 11.68%" }}>
-        <svg style={{ position: "absolute", display: "block", width: "100%", height: "100%" }} fill="none" preserveAspectRatio="none" viewBox="0 0 14.025 13.9936">
-          <path d={svgPaths.pc6fe600} fill="#262626" />
-        </svg>
-      </div>
-      <div style={{ position: "absolute", inset: "0.36% 39.56% 0.39% 52.54%" }}>
-        <svg style={{ position: "absolute", display: "block", width: "100%", height: "100%" }} fill="none" preserveAspectRatio="none" viewBox="0 0 10.086 13.8951">
-          <path d={svgPaths.p1c5d400} fill="#262626" />
-        </svg>
-      </div>
+      GANJJ
     </div>
   );
 }
